cfe78579878b4ae308622f1649ac17f32cdf0950
toni9847 <141242197+toni9847@users.noreply.github.com>
2023-11-22 19:04:34 +0000

    Atualização

---
+++ CP/kanban.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Recuperar dados armazenados localmente
+    const currentVehicle = JSON.parse(localStorage.getItem('currentVehicle'));
+  
+    if (currentVehicle) {
+      // Criar um cartão para o veículo em trânsito
+      const card = createCard(currentVehicle);
+  
+      // Adicionar o cartão à coluna "Aberto"
+      document.getElementById('aberto').appendChild(card);
+  
+      // Adicionar evento de clique no cartão
+      card.addEventListener('click', function() {
+        // Redirecionar para a página principal para completar as informações
+        window.location.href = 'index.html';
+      });
+    }
+  });
+  
+  // Função para criar um cartão com os dados do veículo
+  function createCard(vehicleData) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+  
+    card.innerHTML = `
+      <p><strong>Placa:</strong> ${vehicleData.placa}</p>
+      <p><strong>Motorista:</strong> ${vehicleData.motorista}</p>
+      <p><strong>Rota:</strong> ${vehicleData.rota}</p>
+      <p><strong>Horário de Saída:</strong> ${vehicleData.horarioSaida}</p>
+      <p><strong>Km de Saída:</strong> ${vehicleData.kmSaida}</p>
+    `;
+  
+    return card;
+  }
+  
--- CP/script.js
+++ CP/script.js
@@ -1,25 +1,24 @@
 document.getElementById('vehicleForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    const placa = document.getElementById('placa').value;
-    const motorista = document.getElementById('motorista').value;
-    const rota = document.getElementById('rota').value;
-    const horarioSaida = document.getElementById('horarioSaida').value;
-    const horarioChegada = document.getElementById('horarioChegada').value;
-    const kmSaida = document.getElementById('kmSaida').value;
-    const kmChegada = document.getElementById('kmChegada').value;
-  
-    // Aqui você pode enviar os dados para o backend ou fazer o que for necessário com eles
-  
-    // Exemplo de exibição dos dados
-    alert(`
-      Placa do Veículo: ${placa}
-      Motorista: ${motorista}
-      Rota: ${rota}
-      Horário de Saída: ${horarioSaida}
-      Horário de Chegada: ${horarioChegada}
-      Km de Saída: ${kmSaida}
-      Km de Chegada: ${kmChegada}
-    `);
-  });
-  
+  event.preventDefault();
+
+  const placa = document.getElementById('placa').value;
+  const motorista = document.getElementById('motorista').value;
+  const rota = document.getElementById('rota').value;
+  const horarioSaida = document.getElementById('horarioSaida').value;
+  const kmSaida = document.getElementById('kmSaida').value;
+
+  // Criar objeto com dados do veículo
+  const vehicleData = {
+    placa: placa,
+    motorista: motorista,
+    rota: rota,
+    horarioSaida: horarioSaida,
+    kmSaida: kmSaida
+  };
+
+  // Armazenar dados localmente (pode ser substituído por envio para um servidor)
+  localStorage.setItem('currentVehicle', JSON.stringify(vehicleData));
+
+  // Redirecionar para a página do Kanban
+  window.location.href = 'kanban.html';
+});
