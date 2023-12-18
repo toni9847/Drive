7e5fc925ee6bb3b222b32963f78fc77af3336366
toni9847 <141242197+toni9847@users.noreply.github.com>
2023-12-18 13:07:52 +0000

    Atualização

--- calculo_rescisao/rescisao_app/forms.py
+++ calculo_rescisao/rescisao_app/forms.py
@@ -4,4 +4,4 @@ from .models import Funcionario
 class FuncionarioForm(forms.ModelForm):
     class Meta:
         model = Funcionario
-        fields = ['nome', 'other_field_name', 'another_field_name']
+        fields = ['nome']
